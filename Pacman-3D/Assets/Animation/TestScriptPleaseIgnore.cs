9c915a1ffb0bfdb56e0251d6003346c7e66a9d62
teoAlivanoglou <teoalivanoglou@gmail.com>
2018-01-13 16:54:08 +0200

    Branch test

---
+++ Pacman-3D/Assets/Animation/TestScriptPleaseIgnore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptPleaseIgnore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("100% useless script");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
